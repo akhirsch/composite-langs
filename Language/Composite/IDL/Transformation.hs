4589cfc51cf77b506c890d88e55523dbdf871327
Andrew Hirsch <akhirsch@gwmail.gwu.edu>
2013-02-04 22:04:31 -0500

    Got a transformation that works in all expected first-pass ways except for treating spdid_t as special. Will work on that.

--- Language/Composite/IDL/Transformation.hs
+++ Language/Composite/IDL/Transformation.hs
@@ -4,109 +4,168 @@
 
 module Language.Composite.IDL.Transformation where
   import Language.Pony
+  import Generics.Regular.Rewriting
+  import Language.Pony.Rewriting
   
   type Spdid = Maybe Name
   type StringLength = (Name, Name)
-  type Field = (SType, String)
+  type CField = (SType, String)
   
-  type Fields = (Spdid, [StringLength], [Field])
+  type CFields = (Spdid, [StringLength], [CField])
 
-  createStubStructInfo' :: Name -> [Field] -> CompositeInfo
-  createStubStructInfo' _ [] = ConmpositeInfo Struct Nothing []
+  createStubStructInfo' :: Name -> [CField] -> CompositeInfo
+  createStubStructInfo' _ [] = CompositeInfo Struct Nothing []
   createStubStructInfo' fname fields = let name = "__sg_" ++ fname ++ "_data"
                                            fname' = Just name
-                                           fn t n = Field (Just n) t Nothing
+                                           fn (t, n) = Field (Just n) t Nothing
                                            fields' = map fn fields
                                        in
-                                        CompositeInfo Struct (Just fname') fields'
+                                        CompositeInfo Struct fname' fields'
                                         
   createStubStructInfo :: Name -> CompositeInfo
   createStubStructInfo fname = let name = "__sg_" ++ fname ++ "_data"
                                    fname' = Just name
                                in
-                                CompositeInfo Struct (Just fname') []
+                                CompositeInfo Struct fname' []
 
-  createStubStruct :: Name -> [Field] -> GComposite
-  createStubStruct = GComposite . createStubStructInfo'
+  createStubStruct :: Name -> [CField] -> SGlobal
+  createStubStruct n cf = GComposite $ createStubStructInfo' n cf
   
   -- Assumes that every string has an integer following it that includes the length. 
   getStringLengths :: [(SType, String)] -> [StringLength]
   getStringLengths [] = []
-  getStringLengths ((SPointerTo (SChar _), str) : (SInt _ _, len) : xs) = 
+  getStringLengths ((SPointerTo (SChar _ _) _, str) : (SInt _ _, len) : xs) = 
     (str, len) : getStringLengths xs
-  getStringLengths ((SPointerTo (SChar _), _) : _) = error "String without length (1)"
+  getStringLengths ((SPointerTo (SChar _ _) _,  _) : _) = error "String without length (1)"
   getStringLengths (_ : xs) = getStringLengths xs
   
   getSpdid :: [(SType, String)] ->  Spdid
   getSpdid ((SBuiltinType "spdid_t" [], name) : xs) = Just name
   getSpdid _ = Nothing
   
-  getFieldList :: [(SType, String)] -> [Field]
-  getFieldList [] = []
-  getFieldList as@((t, _) : xs) = let
+  getCFieldList :: [(SType, String)] -> [CField]
+  getCFieldList [] = []
+  getCFieldList as@((t, _) : xs) = let
     -- Assumes that there is no spdid
-    getFieldList' :: [(SType, String)] -> [Field]
-    getFieldList' ((SPointerTo (SChar _), str) : (SInt _ _, len) : xs) = 
-      getFieldList' xs
-    getFieldList' (x : xs) = x : getFieldList' xs
+    getCFieldList' :: [(SType, String)] -> [CField]
+    getCFieldList' ((SPointerTo (SChar _ _) _, str) : (SInt _ _, len) : xs) = 
+      getCFieldList' xs
+    getCFieldList' (x : xs) = x : getCFieldList' xs
+    getCFieldList' [] = []
     in
-     if t == SBuiltinType "spdid_t"
-     then getFieldList' xs
-     else getFieldList' as
+     if t == SBuiltinType "spdid_t" []
+     then getCFieldList' xs
+     else getCFieldList' as
+  
+  
+  getCFields :: [(SType, String)] -> CFields
+  getCFields fs =
+    let spdid = getSpdid fs
+        strLens = getStringLengths fs
+        fieldL = getCFieldList fs
+    in
+     (spdid, strLens, fieldL)
   
   lengthInstruction :: Name -> [StringLength] -> Local
-  lengthInstruction name [] = let struct = createStubStructInfo name fields
-                                  t = SComposite struct
+  lengthInstruction name [] = let struct = SComposite (createStubStructInfo name) []
+  
                            in
-                            (SizeofSType t)
-  lengthInstruction name lens = let struct = createStubStructInfo name fields
-                                    t = SComposite struct
-                                    bin l r = Binary (Ident l) "+" (Ident r)
+                            LStatement . ExpressionS $ (SizeOfSType struct)
+  lengthInstruction name lens = let struct = createStubStructInfo name 
+                                    t = SComposite struct []
+                                    bin l r = Binary (Ident l) "+" r
                                     lengths = map snd lens
-                                    expr = foldr bin (SizeofSType t) length
+                                    expr = foldr bin (SizeOfSType t) lengths
                                     var = Variable "sz" unsignedInt (Just expr)
                                 in 
                                  LDeclaration var
   
   createStubCode :: Name -> SType -> [(SType, String)] -> Function
-  createStubCode fname rtype fields = 
+  createStubCode fname rtype fs = 
     let 
       params = map (\(t, n) -> Parameter (Just n) t) fields
       fname' = fname ++ "_call"
-      structType = SComposite $ createStubStructInfo fname fields
+      structType = SComposite (createStubStructInfo fname) []
       intZero = Literal . CInteger $ 0
-      asserts = stringAsserts fields
-      placements = placeFields fields
+      intRet = Literal . CInteger $ -6
+      f@(spdid, strLengs, fields) = getCFields fs
+      asserts = stringAsserts strLengs
+      placements = placeCFields fields
       instructions1 = [
                         LDeclaration $ Variable "fault" longSignedInt (Just intZero)
                       , LDeclaration $ Variable "ret" rtype Nothing
-                      , LDeclaration $ Variable "d" (SPointerTo structType) Nothing
-                      , LDeclaration $ Variable "cb" (SBuiltinType "cbuf") Nothing
-                      , lengthInstruction fname fields
+                      , LDeclaration $ Variable "d" (SPointerTo structType []) Nothing
+                      , LDeclaration $ Variable "cb" (SBuiltinType "cbuf" []) Nothing
+                      , lengthInstruction fname strLengs
+                      ]
+      cbPtr = Unary "&" (Ident "cb")
+      allocCall = FunctionCall (Ident "cbuf_alloc") [Ident "sz", cbPtr]
+      instructions2 = [
+                        LStatement . ExpressionS $ Binary (Ident "d") "=" allocCall
+                      , LStatement $ IfThen (Unary "!" (Ident "d")) (Return $ Just intRet)
+                      ]
+      dPtr = Unary "&" (Ident "d")
+      dataZero = Brackets (Ident "data") intZero
+      dData = Binary dPtr "->" dataZero
+      spdList = case spdid of
+        Nothing -> []
+        Just spd -> [Ident spd]
+      asmParams = [Ident fname] ++ spdList ++ [Ident "cb", Ident "sz"]
+      memcpyCall = FunctionCall (Ident "memcpy") [dData, Ident "param", Ident "len"]
+      asm = FunctionCall (Ident "CSTUB_ASM_3") asmParams
+      instructions3 = [
+                        LStatement . ExpressionS $ memcpyCall
+                      , LStatement . ExpressionS $ asm
                       ]
                       
-      instructions2 = [
-                      ]
-      instructions = instructions1 ++ instructions2 ++ asserts ++ placements
+      instructions = instructions1 ++ asserts ++ instructions2 
+                       ++ placements ++ instructions3
                       
         
     in
-     Function [] rtype fname' params instructions
+     Function [] rtype fname' params instructions False
 
-  stringAsserts :: [StringLengths] -> [Local]
+  stringAsserts :: [StringLength] -> [Local]
   stringAsserts [] = []
-  stringAsserts ((SPointerTo (SChar _), str) : (SInt _ _, len) : xs) = 
+  stringAsserts ((str, len) : xs) = 
     let intZero = Literal . CInteger $ 0
         bool1 = Binary (Ident str) "&&" (Binary (Ident len) ">=" intZero)
         bool2 = Binary (Brackets (Ident str) (Ident len)) "==" intZero
-        f1 = FunctionCall (Ident "assert") [bool1]
-        f2 = FunctionCall (Ident "assert") [bool2]
+        f = LStatement . ExpressionS
+        f1 = f $ FunctionCall (Ident "assert") [bool1]
+        f2 = f $ FunctionCall (Ident "assert") [bool2]
     in
      [f1, f2] ++ stringAsserts xs
-  stringAsserts ((SPointerTo (SChar _), _) : _) = error "String without length (2)"
-  stringAsserts (_ : xs) = stringAsserts xs
   
-  placeFields :: Fields -> [Local]
-  placeFields [] = []
-  placeFields ((_, name) : xs) = 
-    Binary (Binary (Ident "d") "->" (Ident name)) "=" (Ident name) : placeFields xs
+  placeCFields :: [CField] -> [Local]
+  placeCFields [] = []
+  placeCFields ((_, name) : xs) = 
+    let d = Ident "d"
+        name = Ident "name"
+        dName = Binary d "->" name
+        assgn = LStatement . ExpressionS $ Binary dName "=" name
+    in 
+     assgn : placeCFields xs
+    
+  getCFieldsFromParameters :: [Parameter] -> [(SType, String)]
+  getCFieldsFromParameters [] = []
+  getCFieldsFromParameters ((Parameter (Just n) t) : params) = (t, n) : getCFieldsFromParameters params  
+  getCFieldsFromParameters ((Parameter Nothing t) : params) = (t, "") : getCFieldsFromParameters params
+
+  prototypeToFunction :: [SGlobal] -> [SGlobal]
+  prototypeToFunction [] = []
+  prototypeToFunction ((GFunctionPrototype t n params _) : xs) = 
+    let params' = getCFieldsFromParameters params
+        function = GFunction $ createStubCode n t params'
+        struct = GComposite $ createStubStructInfo' n (getCFieldList params')
+    in
+     [struct, function] ++ (prototypeToFunction xs)
+  prototypeToFunction (x : xs) = x : (prototypeToFunction xs)
+
+  prototypeT :: GenericT
+  prototypeT = mkT prototypeToFunction
+  
+  main :: IO ()
+  main = run $ pony {
+    transformations = [MkTrans "prototype" TopDown prototypeT]
+    }
---
+++ Language/Composite/IDL/test.pony.c
@@ -0,0 +1,5 @@
+typedef int spdid_t;
+typedef int td_t;
+typedef int tor_flags_t;
+
+td_t tsplit(spdid_t spdid, td_t tid, char *param, int len, tor_flags_t tflags, long evtid);
