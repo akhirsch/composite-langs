b629cffd15153fc8f85572b951dded3404da6c60
Andrew Hirsch <akhirsch@gwmail.gwu.edu>
2013-04-06 18:09:50 -0400

    Added output for header.

--- Language/Composite/IDL/Header.hs
+++ Language/Composite/IDL/Header.hs
@@ -3,18 +3,25 @@
 module Main where
   import Language.Pony
   import Language.Composite.IDL
-  
+  import Data.Char
+
   importStatement :: Fix Sem -> [Fix Sem]
   importStatement (µ -> FunCall (Fix ( Name "cidl_import")) [(Fix (CStr str))]) = [include' str]
+  importStatement (µ -> FunCall (Fix (Name "cidl_outport")) [(Fix (CStr s))]) = [define' $ (map toUpper s) ++ "_H"]  
   importStatement (µ -> Typedef {ttype=_, tname=_}) = []
   importStatement (µ -> Pre _) = []
   importStatement (µ -> Post _) = []
   importStatement x = [x] 
   
+
+  outportStatement _ = []
+  
+  
+
   cidlToHeader :: Fix Sem -> Fix Sem
   cidlToHeader (µ -> Program  stmts) = let
                 instrs = concatMap importStatement stmts
-                instrs' = [define' "TORRENT_H"] ++ instrs ++ [endif']
+                instrs' = instrs ++ [endif']
            in
       program' instrs'
   cidlToHeader x = x
